0a3de3bf00f31babde2dff76042f07a59ee26908
kjs study room <101338534+kjs-sr@users.noreply.github.com>
2025-03-13 09:53:19 +0900

    Add files via upload

---
+++ 2025_1_Csharp/Week02/Week02Proj01/FormMain.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Week02Proj01
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput1 = new System.Windows.Forms.TextBox();
+            this.tbxInput2 = new System.Windows.Forms.TextBox();
+            this.tbxInput3 = new System.Windows.Forms.TextBox();
+            this.tbxInput4 = new System.Windows.Forms.TextBox();
+            this.tbxInput5 = new System.Windows.Forms.TextBox();
+            this.chkToggle = new System.Windows.Forms.CheckBox();
+            this.btnoutput01 = new System.Windows.Forms.Button();
+            this.btnoutput02 = new System.Windows.Forms.Button();
+            this.btnoutput03 = new System.Windows.Forms.Button();
+            this.btnoutput04 = new System.Windows.Forms.Button();
+            this.btnoutput05 = new System.Windows.Forms.Button();
+            this.btnoutput06 = new System.Windows.Forms.Button();
+            this.btnoutput07 = new System.Windows.Forms.Button();
+            this.btnoutput08 = new System.Windows.Forms.Button();
+            this.btnoutput09 = new System.Windows.Forms.Button();
+            this.btnoutput10 = new System.Windows.Forms.Button();
+            this.btnoutput11 = new System.Windows.Forms.Button();
+            this.btnoutput12 = new System.Windows.Forms.Button();
+            this.btnoutput13 = new System.Windows.Forms.Button();
+            this.btnoutput14 = new System.Windows.Forms.Button();
+            this.btnoutput15 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxInput1
+            // 
+            this.tbxInput1.Location = new System.Drawing.Point(12, 12);
+            this.tbxInput1.Name = "tbxInput1";
+            this.tbxInput1.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput1.TabIndex = 0;
+            // 
+            // tbxInput2
+            // 
+            this.tbxInput2.Location = new System.Drawing.Point(118, 12);
+            this.tbxInput2.Name = "tbxInput2";
+            this.tbxInput2.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput2.TabIndex = 0;
+            // 
+            // tbxInput3
+            // 
+            this.tbxInput3.Location = new System.Drawing.Point(224, 12);
+            this.tbxInput3.Name = "tbxInput3";
+            this.tbxInput3.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput3.TabIndex = 0;
+            // 
+            // tbxInput4
+            // 
+            this.tbxInput4.Location = new System.Drawing.Point(330, 12);
+            this.tbxInput4.Name = "tbxInput4";
+            this.tbxInput4.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput4.TabIndex = 0;
+            // 
+            // tbxInput5
+            // 
+            this.tbxInput5.Location = new System.Drawing.Point(436, 12);
+            this.tbxInput5.Name = "tbxInput5";
+            this.tbxInput5.Size = new System.Drawing.Size(100, 21);
+            this.tbxInput5.TabIndex = 0;
+            // 
+            // chkToggle
+            // 
+            this.chkToggle.AutoSize = true;
+            this.chkToggle.Location = new System.Drawing.Point(542, 17);
+            this.chkToggle.Name = "chkToggle";
+            this.chkToggle.Size = new System.Drawing.Size(15, 14);
+            this.chkToggle.TabIndex = 1;
+            this.chkToggle.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput01
+            // 
+            this.btnoutput01.Location = new System.Drawing.Point(12, 39);
+            this.btnoutput01.Name = "btnoutput01";
+            this.btnoutput01.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput01.TabIndex = 2;
+            this.btnoutput01.Text = "가감1";
+            this.btnoutput01.UseVisualStyleBackColor = true;
+            this.btnoutput01.Click += new System.EventHandler(this.btnoutput01_Click);
+            // 
+            // btnoutput02
+            // 
+            this.btnoutput02.Location = new System.Drawing.Point(118, 39);
+            this.btnoutput02.Name = "btnoutput02";
+            this.btnoutput02.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput02.TabIndex = 2;
+            this.btnoutput02.Text = "가감2";
+            this.btnoutput02.UseVisualStyleBackColor = true;
+            this.btnoutput02.Click += new System.EventHandler(this.btnoutput02_Click);
+            // 
+            // btnoutput03
+            // 
+            this.btnoutput03.Location = new System.Drawing.Point(224, 39);
+            this.btnoutput03.Name = "btnoutput03";
+            this.btnoutput03.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput03.TabIndex = 2;
+            this.btnoutput03.Text = "가감3";
+            this.btnoutput03.UseVisualStyleBackColor = true;
+            this.btnoutput03.Click += new System.EventHandler(this.btnoutput03_Click);
+            // 
+            // btnoutput04
+            // 
+            this.btnoutput04.Location = new System.Drawing.Point(330, 39);
+            this.btnoutput04.Name = "btnoutput04";
+            this.btnoutput04.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput04.TabIndex = 2;
+            this.btnoutput04.Text = "가감4";
+            this.btnoutput04.UseVisualStyleBackColor = true;
+            this.btnoutput04.Click += new System.EventHandler(this.btnoutput04_Click);
+            // 
+            // btnoutput05
+            // 
+            this.btnoutput05.Location = new System.Drawing.Point(436, 39);
+            this.btnoutput05.Name = "btnoutput05";
+            this.btnoutput05.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput05.TabIndex = 2;
+            this.btnoutput05.Text = "문자와문자열";
+            this.btnoutput05.UseVisualStyleBackColor = true;
+            this.btnoutput05.Click += new System.EventHandler(this.btnoutput05_Click);
+            // 
+            // btnoutput06
+            // 
+            this.btnoutput06.Location = new System.Drawing.Point(12, 71);
+            this.btnoutput06.Name = "btnoutput06";
+            this.btnoutput06.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput06.TabIndex = 2;
+            this.btnoutput06.Text = "형변환";
+            this.btnoutput06.UseVisualStyleBackColor = true;
+            this.btnoutput06.Click += new System.EventHandler(this.btnoutput06_Click);
+            // 
+            // btnoutput07
+            // 
+            this.btnoutput07.Location = new System.Drawing.Point(118, 71);
+            this.btnoutput07.Name = "btnoutput07";
+            this.btnoutput07.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput07.TabIndex = 2;
+            this.btnoutput07.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput08
+            // 
+            this.btnoutput08.Location = new System.Drawing.Point(224, 71);
+            this.btnoutput08.Name = "btnoutput08";
+            this.btnoutput08.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput08.TabIndex = 2;
+            this.btnoutput08.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput09
+            // 
+            this.btnoutput09.Location = new System.Drawing.Point(330, 71);
+            this.btnoutput09.Name = "btnoutput09";
+            this.btnoutput09.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput09.TabIndex = 2;
+            this.btnoutput09.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput10
+            // 
+            this.btnoutput10.Location = new System.Drawing.Point(436, 71);
+            this.btnoutput10.Name = "btnoutput10";
+            this.btnoutput10.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput10.TabIndex = 2;
+            this.btnoutput10.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput11
+            // 
+            this.btnoutput11.Location = new System.Drawing.Point(12, 103);
+            this.btnoutput11.Name = "btnoutput11";
+            this.btnoutput11.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput11.TabIndex = 2;
+            this.btnoutput11.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput12
+            // 
+            this.btnoutput12.Location = new System.Drawing.Point(118, 103);
+            this.btnoutput12.Name = "btnoutput12";
+            this.btnoutput12.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput12.TabIndex = 2;
+            this.btnoutput12.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput13
+            // 
+            this.btnoutput13.Location = new System.Drawing.Point(224, 103);
+            this.btnoutput13.Name = "btnoutput13";
+            this.btnoutput13.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput13.TabIndex = 2;
+            this.btnoutput13.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput14
+            // 
+            this.btnoutput14.Location = new System.Drawing.Point(330, 103);
+            this.btnoutput14.Name = "btnoutput14";
+            this.btnoutput14.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput14.TabIndex = 2;
+            this.btnoutput14.UseVisualStyleBackColor = true;
+            // 
+            // btnoutput15
+            // 
+            this.btnoutput15.Location = new System.Drawing.Point(436, 103);
+            this.btnoutput15.Name = "btnoutput15";
+            this.btnoutput15.Size = new System.Drawing.Size(100, 26);
+            this.btnoutput15.TabIndex = 2;
+            this.btnoutput15.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.White;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(13, 136);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(523, 305);
+            this.lblResult.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnoutput15);
+            this.Controls.Add(this.btnoutput10);
+            this.Controls.Add(this.btnoutput05);
+            this.Controls.Add(this.btnoutput14);
+            this.Controls.Add(this.btnoutput09);
+            this.Controls.Add(this.btnoutput04);
+            this.Controls.Add(this.btnoutput13);
+            this.Controls.Add(this.btnoutput12);
+            this.Controls.Add(this.btnoutput08);
+            this.Controls.Add(this.btnoutput07);
+            this.Controls.Add(this.btnoutput11);
+            this.Controls.Add(this.btnoutput03);
+            this.Controls.Add(this.btnoutput06);
+            this.Controls.Add(this.btnoutput02);
+            this.Controls.Add(this.btnoutput01);
+            this.Controls.Add(this.chkToggle);
+            this.Controls.Add(this.tbxInput5);
+            this.Controls.Add(this.tbxInput4);
+            this.Controls.Add(this.tbxInput3);
+            this.Controls.Add(this.tbxInput2);
+            this.Controls.Add(this.tbxInput1);
+            this.Name = "FormMain";
+            this.Text = "2주차";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput1;
+        private System.Windows.Forms.TextBox tbxInput2;
+        private System.Windows.Forms.TextBox tbxInput3;
+        private System.Windows.Forms.TextBox tbxInput4;
+        private System.Windows.Forms.TextBox tbxInput5;
+        private System.Windows.Forms.CheckBox chkToggle;
+        private System.Windows.Forms.Button btnoutput01;
+        private System.Windows.Forms.Button btnoutput02;
+        private System.Windows.Forms.Button btnoutput03;
+        private System.Windows.Forms.Button btnoutput04;
+        private System.Windows.Forms.Button btnoutput05;
+        private System.Windows.Forms.Button btnoutput06;
+        private System.Windows.Forms.Button btnoutput07;
+        private System.Windows.Forms.Button btnoutput08;
+        private System.Windows.Forms.Button btnoutput09;
+        private System.Windows.Forms.Button btnoutput10;
+        private System.Windows.Forms.Button btnoutput11;
+        private System.Windows.Forms.Button btnoutput12;
+        private System.Windows.Forms.Button btnoutput13;
+        private System.Windows.Forms.Button btnoutput14;
+        private System.Windows.Forms.Button btnoutput15;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
